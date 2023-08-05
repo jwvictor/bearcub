fc4f07411a258c378e63dd330e85b80714044131
Jason Victor <jason.w.victor@gmail.com>
2023-08-05 13:26:47 +0000

    commented out old data model

--- src/server/provider.rs
+++ src/server/provider.rs
@@ -6,7 +6,7 @@ use anyhow::*;
 pub struct Provider {
     data_dir: String,
     user_id: String,
-    blob_root: Option<BlobNodeRef>,
+    blob_root: Option<SkeletonNode>,
 }
 
 impl Provider {
@@ -26,15 +26,15 @@ impl Provider {
 
     }
 
-    pub fn check_root_structure(&mut self) {
-        if self.blob_root.is_some() {
-            return;
-        }
-
-        let filename = self.skeleton_filename();
-        self.blob_root = BlobNode::from_file(&filename[..]).ok();
-    }
-
+    // pub fn check_root_structure(&mut self) {
+    //     if self.blob_root.is_some() {
+    //         return;
+    //     }
+    //
+    //     let filename = self.skeleton_filename();
+    //     self.blob_root = BlobNode::from_file(&filename[..]).ok();
+    // }
+    //
     pub fn check_storage_ready(&self) -> anyhow::Result<()> {
         let path = self.user_data_path();
         if !Path::new(&path[..]).exists() {
@@ -46,98 +46,100 @@ impl Provider {
         }
     }
 
-    pub fn flush(&mut self) -> Result<()> {
-        self.check_root_structure();
-        self.check_storage_ready()?;
-        match &self.blob_root {
-            Some(root) => root.flush_to_file(&self.skeleton_filename()[..]),
-            None => Ok(()),
-        }
-    }
-
-    pub fn put_blob(&mut self, id: &str, title: &str, parent_id: Option<&str>) -> Result<()> {
-        match parent_id {
-            None => {
-                match &mut self.blob_root {
-                    Some(root) => {
-                        root.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
-                        Ok(())
-                    },
-                    None => Ok(())
-                }
-            },
-            Some(pid) => {
-                match &mut self.blob_root {
-                    Some(root) => {
-                        // let mut parent: &mut BlobNode = by_id_for_node(&root, pid).unwrap();
-                        // parent.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
-                        Ok(())
-
-                    },
-                    None => Err(anyhow!("No skeleton loaded")),
-                }
-            }
-        }
-    }
-
-    pub fn get_blob(&self, id: &str) -> Option<BlobNodeRef> {
-        match &self.blob_root {
-            Some(root) => {
-                let res = by_id_for_node(root.clone(), id);
-                if res.is_some() {
-                    Some(res.unwrap().clone())
-                } else {
-                    None
-                }
-            },
-            None => None,
-        }
-    }
+    // pub fn flush(&mut self) -> Result<()> {
+    //     self.check_root_structure();
+    //     self.check_storage_ready()?;
+    //     match &self.blob_root {
+    //         Some(root) => root.flush_to_file(&self.skeleton_filename()[..]),
+    //         None => Ok(()),
+    //     }
+    // }
+    //
+    // pub fn put_blob(&mut self, id: &str, title: &str, parent_id: Option<&str>) -> Result<()> {
+    //     match parent_id {
+    //         None => {
+    //             match &mut self.blob_root {
+    //                 Some(root) => {
+    //                     root.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
+    //                     Ok(())
+    //                 },
+    //                 None => Ok(())
+    //             }
+    //         },
+    //         Some(pid) => {
+    //             match &mut self.blob_root {
+    //                 Some(root) => {
+    //                     // let mut parent: &mut BlobNode = by_id_for_node(&root, pid).unwrap();
+    //                     // parent.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
+    //                     Ok(())
+    //
+    //                 },
+    //                 None => Err(anyhow!("No skeleton loaded")),
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // pub fn get_blob(&self, id: &str) -> Option<BlobNodeRef> {
+    //     match &self.blob_root {
+    //         Some(root) => {
+    //             let res = by_id_for_node(root.clone(), id);
+    //             if res.is_some() {
+    //                 Some(res.unwrap().clone())
+    //             } else {
+    //                 None
+    //             }
+    //         },
+    //         None => None,
+    //     }
+    // }
 }
 
 
-fn by_id_for_node<'a>(node: BlobNodeRef, id: &str) -> Option<BlobNodeRef> {
-    let node_borrow = node.node();
-    if (*node_borrow.borrow()).id().eq(id) {
-        // let rig = node.clone();
-        Some(node)
-    } else {
-        let c = (*node_borrow.borrow()).children();
-        for x in c {
-            let rv = by_id_for_node(x, id);
-            if rv.is_some() {
-                // return Some(rv.unwrap().clone())
-                return rv;
-            }
-        }
-        return None;
-    }
-}
-
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_by_id() {
-        const ID0: &str = "0";
-        const ROOT: &str = "root";
-        const ID1: &str = "1";
-        const TITLE1: &str = "notes";
-        const ID2: &str = "2";
-        const TITLE2: &str = "passwords";
-        let bc1 = BlobNode::new(ID1.to_string(), TITLE1.to_string(), vec![]);
-        let bc2 = BlobNode::new(ID2.to_string(), TITLE2.to_string(), vec![]);
-        let bc0 = BlobNode::new(ID0.to_string(), ROOT.to_string(), vec![bc1, bc2]);
-        let _ = bc0.flush_to_file("data/test1/blobs.bson");
-        let mut provider = Provider::new("./data".to_string(), "test1".to_string());
-        provider.check_root_structure();
-        let blob = provider.get_blob(ID1);
-        assert_eq!(blob.is_some(), true);
-        assert_eq!(blob.is_some(), true);
-
-
-    }
-}
+// fn by_id_for_node<'a>(node: BlobNodeRef, id: &str) -> Option<BlobNodeRef> {
+//     let n = node.node();
+//     let node_borrow = (*n).borrow();
+//     if node_borrow.id().eq(id) {
+//         // let rig = node.clone();
+//         Some(node)
+//     } else {
+//         println!("{} is not {}", node_borrow.id(), id);
+//         let c = node_borrow.children();
+//         for x in c {
+//             let rv = by_id_for_node(x, id);
+//             if rv.is_some() {
+//                 // return Some(rv.unwrap().clone())
+//                 return rv;
+//             }
+//         }
+//         return None;
+//     }
+// }
+//
+//
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_get_by_id() {
+//         const ID0: &str = "0";
+//         const ROOT: &str = "root";
+//         const ID1: &str = "1";
+//         const TITLE1: &str = "notes";
+//         const ID2: &str = "2";
+//         const TITLE2: &str = "passwords";
+//         let bc1 = BlobNode::new(ID1.to_string(), TITLE1.to_string(), vec![]);
+//         let bc2 = BlobNode::new(ID2.to_string(), TITLE2.to_string(), vec![]);
+//         let bc0 = BlobNode::new(ID0.to_string(), ROOT.to_string(), vec![bc1, bc2]);
+//         let _ = bc0.flush_to_file("data/test1/blobs.bson");
+//         let mut provider = Provider::new("./data".to_string(), "test1".to_string());
+//         provider.check_root_structure();
+//         let blob = provider.get_blob(ID1);
+//         assert_eq!(blob.is_some(), true);
+//         assert_eq!(blob.is_some(), true);
+//
+//
+//     }
+// }
--- src/storage/format.rs
+++ src/storage/format.rs
@@ -8,110 +8,127 @@ use std::io::prelude::*;
 use std::rc::Rc;
 use bson::*;
 
+
+
+
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SkeletonNode {
+    id: String,
+    title: String,
+    child_ids: Vec<usize>,
+}
+
+
+
+
+
+
 // Step 1: maintain a file with the hierarchy of blobs, to store in <user id>/blobs.bson
 // Step 2: maintain individual files for blobs at <user id>/<blob id>.json
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct BlobNodeRef {
-    node: Rc<RefCell<BlobNode>>,
-}
-
-
-// The root blob node owns all child blobs.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct BlobNode {
-  id: String,
-  title: String,
-  children: Vec<BlobNodeRef>,
-}
-
-impl BlobNode {
-    pub fn new(id: String, title: String, children: Vec<BlobNode>) -> BlobNode {
-        let children_refs:Vec<BlobNodeRef> = children.into_iter().map(|x| BlobNodeRef{node: Rc::new(RefCell::new(x))}).collect();
-        return BlobNode{id, title, children: children_refs};
-    }
-
-    pub fn id(&self) -> &str {
-        &self.id[..]
-    }
-
-    pub fn from_file(path: &str) -> Result<BlobNodeRef> {
-        // let fpath = path.to_string();
-        let fp = fs::read(path)?;
-        let deser: BlobNodeRef = bson::from_slice(&fp[..])?;
-        Ok(deser)
-
-    }
-
-    pub fn children(&self) -> Vec<BlobNodeRef> {
-        let mut v = vec![];
-        for c in &self.children {
-            v.push(c.clone());
-        }
-        v
-    }
-
-    pub fn add_child(&mut self, child: BlobNode) {
-        self.children.push(BlobNodeRef{ node: Rc::new(RefCell::new(child))});
-    }
-
-    pub fn flush_to_file(&self, path: &str) -> Result<()> {
-        let mut file = File::create(path)?;
-        let bs_obj = bson::to_bson(&self)?;
-        let bs = bson::to_vec(&bs_obj)?;
-        file.write_all(&bs[..])?;
-        Ok(())
-    }
-
-    pub fn eq(&self, other: BlobNode) -> bool {
-        self.id.eq(&other.id) && self.children.len() == other.children.len()
-    }
-}
-
-impl BlobNodeRef {
-    pub fn from(node: BlobNode) -> BlobNodeRef {
-        BlobNodeRef{ node: Rc::new(RefCell::new(node))}
-    }
-
-    pub fn flush_to_file(&self, path: &str) -> Result<()> {
-        let x = (*self.node).borrow();
-        x.flush_to_file(path)
-    }
-   
-
-    // TODO - everything is going to explode due to the runtime borrow checking of these ref cells.
-    // need to enforce concurrent access in some sane-ish way
-    pub fn add_child(&self, child: BlobNode) {
-        let mut root = (*self.node).borrow_mut();
-        root.add_child(child)
-    }
-
-    pub fn node(&self) -> Rc<RefCell<BlobNode>> {
-        self.node.clone()
-    }
-
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_node_flush() {
-        const ID0: &str = "0";
-        const ROOT: &str = "root";
-        const ID1: &str = "1";
-        const TITLE1: &str = "notes";
-        const ID2: &str = "2";
-        const TITLE2: &str = "passwords";
-        let bc1 = BlobNode::new(ID1.to_string(), TITLE1.to_string(), vec![]);
-        let bc2 = BlobNode::new(ID2.to_string(), TITLE2.to_string(), vec![]);
-        let bc0 = BlobNode::new(ID0.to_string(), ROOT.to_string(), vec![bc1, bc2]);
-        let res = bc0.flush_to_file("testfile.bson");
-        assert_eq!(res.is_ok(), true);
-        let input_file = fs::read("testfile.bson").unwrap();
-        let deserialized: BlobNode = bson::from_slice_utf8_lossy(&input_file).unwrap();
-        assert_eq!(deserialized.eq(bc0), true)
-    }
-}
+// #[derive(Clone, Debug, Serialize, Deserialize)]
+// pub struct BlobNodeRef {
+//     node: Rc<RefCell<BlobNode>>,
+// }
+//
+//
+// // The root blob node owns all child blobs.
+// #[derive(Debug, Serialize, Deserialize, Clone)]
+// pub struct BlobNode {
+//   id: String,
+//   title: String,
+//   children: Vec<BlobNodeRef>,
+// }
+//
+// impl BlobNode {
+//     pub fn new(id: String, title: String, children: Vec<BlobNode>) -> BlobNode {
+//         let children_refs:Vec<BlobNodeRef> = children.into_iter().map(|x| BlobNodeRef{node: Rc::new(RefCell::new(x))}).collect();
+//         return BlobNode{id, title, children: children_refs};
+//     }
+//
+//     pub fn id(&self) -> &str {
+//         &self.id[..]
+//     }
+//
+//     pub fn from_file(path: &str) -> Result<BlobNodeRef> {
+//         // let fpath = path.to_string();
+//         let fp = fs::read(path)?;
+//         let deser: BlobNodeRef = bson::from_slice(&fp[..])?;
+//         Ok(deser)
+//
+//     }
+//
+//     pub fn children(&self) -> Vec<BlobNodeRef> {
+//         let mut v = vec![];
+//         for c in &self.children {
+//             v.push(c.clone());
+//         }
+//         v
+//     }
+//
+//     pub fn add_child(&mut self, child: BlobNode) {
+//         self.children.push(BlobNodeRef{ node: Rc::new(RefCell::new(child))});
+//     }
+//
+//     pub fn flush_to_file(&self, path: &str) -> Result<()> {
+//         let mut file = File::create(path)?;
+//         let bs_obj = bson::to_bson(&self)?;
+//         let bs = bson::to_vec(&bs_obj)?;
+//         file.write_all(&bs[..])?;
+//         Ok(())
+//     }
+//
+//     pub fn eq(&self, other: BlobNode) -> bool {
+//         self.id.eq(&other.id) && self.children.len() == other.children.len()
+//     }
+// }
+//
+// impl BlobNodeRef {
+//     pub fn from(node: BlobNode) -> BlobNodeRef {
+//         BlobNodeRef{ node: Rc::new(RefCell::new(node))}
+//     }
+//
+//     pub fn flush_to_file(&self, path: &str) -> Result<()> {
+//         let x = (*self.node).borrow();
+//         x.flush_to_file(path)
+//     }
+//    
+//
+//     // TODO - everything is going to explode due to the runtime borrow checking of these ref cells.
+//     // need to enforce concurrent access in some sane-ish way
+//     pub fn add_child(&self, child: BlobNode) {
+//         let mut root = (*self.node).borrow_mut();
+//         root.add_child(child)
+//     }
+//
+//     pub fn node(&self) -> Rc<RefCell<BlobNode>> {
+//         self.node.clone()
+//     }
+//
+// }
+//
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_node_flush() {
+//         const ID0: &str = "0";
+//         const ROOT: &str = "root";
+//         const ID1: &str = "1";
+//         const TITLE1: &str = "notes";
+//         const ID2: &str = "2";
+//         const TITLE2: &str = "passwords";
+//         let bc1 = BlobNode::new(ID1.to_string(), TITLE1.to_string(), vec![]);
+//         let bc2 = BlobNode::new(ID2.to_string(), TITLE2.to_string(), vec![]);
+//         let bc0 = BlobNode::new(ID0.to_string(), ROOT.to_string(), vec![bc1, bc2]);
+//         let res = bc0.flush_to_file("testfile.bson");
+//         assert_eq!(res.is_ok(), true);
+//         let input_file = fs::read("testfile.bson").unwrap();
+//         let deserialized: BlobNode = bson::from_slice_utf8_lossy(&input_file).unwrap();
+//         assert_eq!(deserialized.eq(bc0), true)
+//     }
+// }
