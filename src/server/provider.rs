4d162fbcb3e46d104cc9b324dde114a4f4d6548b
Jason Victor <jason.w.victor@gmail.com>
2023-08-06 14:32:23 +0000

    more functions

--- src/server/provider.rs
+++ src/server/provider.rs
@@ -6,12 +6,12 @@ use anyhow::*;
 pub struct Provider {
     data_dir: String,
     user_id: String,
-    blob_root: Option<SkeletonNode>,
+    skeleton: Option<SkeletonHandleRef>,
 }
 
 impl Provider {
     pub fn new(data_dir: String, user_id: String) -> Provider {
-        Provider { data_dir, user_id, blob_root: None }
+        Provider { data_dir, user_id, skeleton: None }
     }
 
     fn user_data_path(&self) -> String {
@@ -26,15 +26,15 @@ impl Provider {
 
     }
 
-    // pub fn check_root_structure(&mut self) {
-    //     if self.blob_root.is_some() {
-    //         return;
-    //     }
-    //
-    //     let filename = self.skeleton_filename();
-    //     self.blob_root = BlobNode::from_file(&filename[..]).ok();
-    // }
-    //
+    pub fn check_root_structure(&mut self) {
+        if self.skeleton.is_some() {
+            return;
+        }
+
+        let filename = self.skeleton_filename();
+        self.skeleton = SkeletonHandleRef::from_file(&filename[..]).ok();
+    }
+
     pub fn check_storage_ready(&self) -> anyhow::Result<()> {
         let path = self.user_data_path();
         if !Path::new(&path[..]).exists() {
@@ -46,19 +46,27 @@ impl Provider {
         }
     }
 
-    // pub fn flush(&mut self) -> Result<()> {
-    //     self.check_root_structure();
-    //     self.check_storage_ready()?;
-    //     match &self.blob_root {
-    //         Some(root) => root.flush_to_file(&self.skeleton_filename()[..]),
-    //         None => Ok(()),
-    //     }
-    // }
-    //
+    pub fn flush(&mut self) -> Result<()> {
+        self.check_root_structure();
+        self.check_storage_ready()?;
+        match &self.skeleton {
+            Some(root) => root.flush_to_file(&self.skeleton_filename()[..]),
+            None => Ok(()),
+        }
+    }
+
+    pub fn get_skeleton_node(&self, id: &str) -> Option<SkeletonNode> {
+        match &self.skeleton {
+            Some(root) => root.get(id),
+            None => None,
+        }
+    }
+
+
     // pub fn put_blob(&mut self, id: &str, title: &str, parent_id: Option<&str>) -> Result<()> {
     //     match parent_id {
     //         None => {
-    //             match &mut self.blob_root {
+    //             match &mut self.skeleton {
     //                 Some(root) => {
     //                     root.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
     //                     Ok(())
@@ -67,7 +75,7 @@ impl Provider {
     //             }
     //         },
     //         Some(pid) => {
-    //             match &mut self.blob_root {
+    //             match &mut self.skeleton {
     //                 Some(root) => {
     //                     // let mut parent: &mut BlobNode = by_id_for_node(&root, pid).unwrap();
     //                     // parent.add_child(BlobNode::new(id.to_string(), title.to_string(), vec![]));
@@ -81,7 +89,7 @@ impl Provider {
     // }
     //
     // pub fn get_blob(&self, id: &str) -> Option<BlobNodeRef> {
-    //     match &self.blob_root {
+    //     match &self.skeleton {
     //         Some(root) => {
     //             let res = by_id_for_node(root.clone(), id);
     //             if res.is_some() {
